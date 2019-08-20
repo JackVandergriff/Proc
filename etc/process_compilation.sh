67536bd2718698384ab2ec817abddcd76d67de1e
JackVandergriff <jackvandergriff02@gmail.com>
2019-08-19 22:36:06 -0400

    delete missing files

--- etc/process_compilation.sh
+++ etc/process_compilation.sh
@@ -11,6 +11,3 @@ gcc process.c -c -o process_c.o
 nvcc process.cu -c -o process_cuda.o
 g++ -o $BIN/process_test -L/opt/cuda/lib -lcuda -lcudart process_cpp.o process_cuda.o process_c.o
 rm *.o
-chmod +x $BIN/process_test
-
-cd $ETC
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "game.hpp"
-
-int main() {
-    Game game;
-
-    game.main();
-
-    return 0;
-}
